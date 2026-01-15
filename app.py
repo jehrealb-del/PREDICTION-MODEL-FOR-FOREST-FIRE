9ec95b0d6088dfb8cf023ec04ba0d4f069d3b072
jehrealb-del <jehrealb@gmail.com>
2026-01-15 04:52:55 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+
+st.subheader("DEBUG OUTPUT")
+
+st.write("Final input DataFrame (model input):")
+st.write(final_input_df)
+
+st.write("Scaled input values:")
+st.write(scaled_input)
+
+st.write("Prediction probabilities:")
+st.write(loaded_model.predict_proba(scaled_input))
+
 import streamlit as st
 import pandas as pd
 import joblib
